5cda04aef3331c545faf96e896c1275d530e59a7
tedwon <iamtedwon@gmail.com>
2013-11-06 12:43:55 +0900

    Add sample classes

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ byteman-quickstart
 ==================
 
 Byteman Quick Start
+
+http://tedwon.com/display/dev/Byteman
+
+sudo ln -s /Users/<USER_HOME>/byteman-2.1.3/ /usr/local/byteman
---
+++ src/main/java/org/jbugkorea/byteman/BytemanTestAppMain.java
@@ -0,0 +1,28 @@
+package org.jbugkorea.byteman;
+
+/**
+ * Byteman Test App Main Class<p/>
+ * <pre>
+ * Run:
+ * $ java -javaagent:/usr/local/byteman/lib/byteman.jar=script:/usr/local/byteman/rules/appmain.btm,listener:true,port:9091,address:127.0.0.1 org.jbugkorea.byteman.BytemanTestAppMain foo bar ted
+ *
+ * Expected:
+ * entering main
+ * foo
+ * bar
+ * ted
+ * exiting main
+ * </pre>
+ *
+ * @author <a href=mailto:iamtedwon@gmail.com">Ted Won</a>
+ * @version 0.1.0
+ * @since 0.1.0
+ */
+public class BytemanTestAppMain {
+
+  public static void main(String[] args) {
+    for (int i = 0; i < args.length; i++) {
+      System.out.println(args[i]);
+    }
+  }
+}
