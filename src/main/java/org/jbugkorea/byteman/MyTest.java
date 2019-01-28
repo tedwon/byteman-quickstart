a4569fcb5472eb7273bc2c6c0859ba3747f7287b
tedwon <jwon@redhat.com>
2019-01-28 17:59:43 +0900

    update example

--- src/main/java/org/jbugkorea/byteman/MyTest.java
+++ src/main/java/org/jbugkorea/byteman/MyTest.java
@@ -1,17 +1,24 @@
 package org.jbugkorea.byteman;
 
 /**
- * -javaagent:/Users/tedwon/runtime/byteman-3.0.10/lib/byteman.jar=script:/Users/tedwon/build/byteman/byteman-quickstart/rules/mytest.btm
+ * -javaagent:/Users/tedwon/byteman/lib/byteman.jar=script:/Users/tedwon/build/byteman/byteman-quickstart/rules/mytest.btm
+ *
+ * <p/>
+ *
+ * -Dorg.jboss.byteman.verbose=true -Dorg.jboss.byteman.debug=true -javaagent:/Users/tedwon/byteman/lib/byteman.jar=script:/Users/tedwon/build/byteman/byteman-quickstart/rules/mytest2.btm
  */
 public class MyTest {
 
    public int a = 1234;
 
-   public static void main(String[] args) {
+   public static void main(String[] args) throws Exception {
       new MyTest().test("a", 2, "c", true);
    }
 
-   private void test(String a, int b, String c, boolean d) {
+   private void test(String a, int b, String c, boolean d) throws Exception {
+      String abc = "abcd";
+
+      throw new Exception("my test ex");
 
    }
 }
